30f1a8dbb08a7d6bc14f0953fdf332ce3b1cad63
SquatchHunter <paskz@hotmail.com>
2024-07-24 15:28:25 +1000

    feat(files): wandering_trader_announcements
    * add reset character to end of tellraw to prevent overflow issues

--- addons/files/informative/wandering_trader_announcements/bp/scripts/index.ts
+++ addons/files/informative/wandering_trader_announcements/bp/scripts/index.ts
@@ -7,7 +7,7 @@ import { world, EntitySpawnAfterEvent } from '@minecraft/server';
 		// filter for just wandering trader spawns
 		if(eventEntity.entity.typeId === "minecraft:wandering_trader"){
 			// send message to all players within 60 blocks of the trader
-			eventEntity.entity.runCommand(`tellraw @e[r=60,type=player] {"rawtext":[{"text":"§6A Wandering Trader has appeared nearby!"}]}`)
+			eventEntity.entity.runCommand(`tellraw @e[r=60,type=player] {"rawtext":[{"text":"§6A Wandering Trader has appeared nearby!§r"}]}`)
 		}
 	});
 })();
